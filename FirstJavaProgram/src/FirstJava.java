4534da061b098dd52a9bb0ff83d6c2dddef09c5e
KARUNA <KARUNA@lova>
2014-10-25 12:49:32 +0530

    FOR LOOP ADD ... IN THE MAIN JAVA PROGRAM....

--- FirstJavaProgram/src/FirstJava.java
+++ FirstJavaProgram/src/FirstJava.java
@@ -2,6 +2,10 @@ public class FirstJava {
 
 	public static void main(String[] args) {
 		System.out.println("hi this is lova.... have a nice day....");
+		for (int i = 0; i < 5; i++) {
 
+			System.out.println(" value of i is  : " + i);
+
+		}
 	}
 }
